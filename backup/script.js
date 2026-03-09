0ed81006dfc31ae1d6990b3f1db618008a780660
niissxy <anisay106@gmail.com>
2026-03-09 15:44:07 +0700

    update

---
+++ backup/script.js
@@ -0,0 +1,493 @@
+// pen flash
+function togglePenFlash () {
+  const section = document.getElementById('pen-flash');
+  const openBtn = document.getElementById('openPenFlashBtn');
+
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closePenFlash () {
+  const section = document.getElementById('pen-flash');
+  const openBtn = document.getElementById('openPenFlashBtn');
+  const target = document.getElementById('pen-flash-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// rubber flash
+function toggleRubber () {
+  const section = document.getElementById('rubber-flash');
+  const openBtn = document.getElementById('openRubberBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeRubber () {
+  const section = document.getElementById('rubber-flash');
+  const openBtn = document.getElementById('openRubberBtn');
+  const target = document.getElementById('rubber-flash-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// bluetooth speaker
+function toggleBluetoothSpeaker () {
+  const section = document.getElementById('bluetooth-speaker');
+  const openBtn = document.getElementById('openSpeakerBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeBluetoothSpeaker () {
+  const section = document.getElementById('bluetooth-speaker');
+  const openBtn = document.getElementById('openSpeakerBtn');
+  const target = document.getElementById('bluetooth-speaker-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// bluetooth headset
+function toggleBluetoothHeadset () {
+  const section = document.getElementById('bluetooth-headset');
+  const openBtn = document.getElementById('openHeadsetBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeBluetoothHeadset () {
+  const section = document.getElementById('bluetooth-headset');
+  const openBtn = document.getElementById('openHeadsetBtn');
+  const target = document.getElementById('headset-bluetooth-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// accesorries custom
+function toggleAccesorries () {
+  const section = document.getElementById('accesorries');
+  const openBtn = document.getElementById('openAccesorriesBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeAccesorries () {
+  const section = document.getElementById('accesorries');
+  const openBtn = document.getElementById('openAccesorriesBtn');
+  const target = document.getElementById('accesorries-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// packaging
+function togglePackaging () {
+  const section = document.getElementById('packaging');
+  const openBtn = document.getElementById('openPackagingBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closePackaging () {
+  const section = document.getElementById('packaging');
+  const openBtn = document.getElementById('openPackagingBtn');
+  const target = document.getElementById('packaging-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// aluminium bottle
+function toggleBottle () {
+  const section = document.getElementById('aluminium-bottle');
+  const openBtn = document.getElementById('openBottleBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeAluminiumBottle () {
+  const section = document.getElementById('aluminium-bottle');
+  const openBtn = document.getElementById('openBottleBtn');
+  const target = document.getElementById('aluminium-bottle-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// office cup
+function toggleOfficeCup () {
+  const section = document.getElementById('office-cup');
+  const openBtn = document.getElementById('openCupBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeOfficeCup () {
+  const section = document.getElementById('office-cup');
+  const openBtn = document.getElementById('openCupBtn');
+  const target = document.getElementById('office-cup-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// food accesorries
+function toggleFoodAccesorries () {
+  const section = document.getElementById('food-accesorries');
+  const openBtn = document.getElementById('openFoodAccesorriesBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeFoodAccesorries () {
+  const section = document.getElementById('food-accesorries');
+  const openBtn = document.getElementById('openFoodAccesorriesBtn');
+  const target = document.getElementById('food-accesorries-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// shirt custom
+function toggleShirt () {
+  const section = document.getElementById('shirt-custom');
+  const openBtn = document.getElementById('openShirtBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeShirt () {
+  const section = document.getElementById('shirt-custom');
+  const openBtn = document.getElementById('openShirtBtn');
+  const target = document.getElementById('shirt-custom-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// outerwear
+function toggleOuterwear () {
+  const section = document.getElementById('outerwear-custom');
+  const openBtn = document.getElementById('openOuterwearBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeOuterwear () {
+  const section = document.getElementById('outerwear-custom');
+  const openBtn = document.getElementById('openOuterwearBtn');
+  const target = document.getElementById('outerwear-custom-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// headwear
+function toggleHeadwear () {
+  const section = document.getElementById('headwear-custom');
+  const openBtn = document.getElementById('openHeadwearBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeHeadwear () {
+  const section = document.getElementById('headwear-custom');
+  const openBtn = document.getElementById('openHeadwearBtn');
+  const target = document.getElementById('headwear-custom-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// holder custom
+function toggleHolder () {
+  const section = document.getElementById('holder-custom');
+  const openBtn = document.getElementById('openHolderBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeHolder () {
+  const section = document.getElementById('holder-custom');
+  const openBtn = document.getElementById('openHolderBtn');
+  const target = document.getElementById('holder-custom-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// functional souvenirs
+function toggleFunctional () {
+  const section = document.getElementById('functional-products');
+  const openBtn = document.getElementById('openFunctionalBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeFunctional () {
+  const section = document.getElementById('functional-products');
+  const openBtn = document.getElementById('openFunctionalBtn');
+  const target = document.getElementById('functional-products-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// plastic pen
+function togglePlasticPen () {
+  const section = document.getElementById('plastic-pen');
+  const openBtn = document.getElementById('openPlasticPenBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closePlasticPen () {
+  const section = document.getElementById('plastic-pen');
+  const openBtn = document.getElementById('openPlasticPenBtn');
+  const target = document.getElementById('plastic-pen-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// metal pen
+function toggleMetalPen () {
+  const section = document.getElementById('metal-pen');
+  const openBtn = document.getElementById('openMetalPenBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeMetalPen () {
+  const section = document.getElementById('metal-pen');
+  const openBtn = document.getElementById('openMetalPenBtn');
+  const target = document.getElementById('metal-pen-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// umbrella
+function toggleUmbrella () {
+  const section = document.getElementById('umbrella');
+  const openBtn = document.getElementById('openUmbrellaBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeUmbrella () {
+  const section = document.getElementById('umbrella');
+  const openBtn = document.getElementById('openUmbrellaBtn');
+  const target = document.getElementById('umbrella-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// clock
+function toggleClock () {
+  const section = document.getElementById('clock');
+  const openBtn = document.getElementById('openClockBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeClock () {
+  const section = document.getElementById('clock');
+  const openBtn = document.getElementById('openClockBtn');
+  const target = document.getElementById('clock-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// id card custom
+function toggleIdCard () {
+  const section = document.getElementById('id-card');
+  const openBtn = document.getElementById('openIdCardBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeIdCard () {
+  const section = document.getElementById('id-card');
+  const openBtn = document.getElementById('openIdCardBtn');
+  const target = document.getElementById('id-card-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// pin keychain custom
+function togglePinKeychain () {
+  const section = document.getElementById('pin-keychain');
+  const openBtn = document.getElementById('openPinKeychainBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closePinKeychain () {
+  const section = document.getElementById('pin-keychain');
+  const openBtn = document.getElementById('openPinKeychainBtn');
+  const target = document.getElementById('pin-keychain-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// print products
+function togglePrintProducts () {
+  const section = document.getElementById('print-products');
+  const openBtn = document.getElementById('openPrintProductsBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closePrintProducts () {
+  const section = document.getElementById('print-products');
+  const openBtn = document.getElementById('openPrintProductsBtn');
+  const target = document.getElementById('print-products-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+// slippers product
+function toggleSlippersProducts () {
+  const section = document.getElementById('slipper-products');
+  const openBtn = document.getElementById('openSlippersBtn');
+  
+  section.style.display = 'block';
+  openBtn.style.display = 'none';
+
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function closeSlipperProducts () {
+  const section = document.getElementById('slipper-products');
+  const openBtn = document.getElementById('openSlippersBtn');
+  const target = document.getElementById('slipper-products-section');
+
+  section.style.display = 'none';
+  openBtn.style.display = 'block';
+
+  target.scrollIntoView({ behavior: 'smooth' });
+}
+
+function toggleDaftarIsi() {
+  const daftarIsi = document.getElementById("daftarIsi");
+
+  if (daftarIsi.style.display === "none") {
+    daftarIsi.style.display = "block";
+  } else {
+    daftarIsi.style.display = "none";
+  }
+}
