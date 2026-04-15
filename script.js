fb799c19d673e5af053f22cbd3da76ee3158fd32
niissxy <anisay106@gmail.com>
2026-04-15 10:37:53 +0700

    update navbar

--- script.js
+++ script.js
@@ -94,3 +94,13 @@ const productGroup = document.querySelectorAll(".product-group");
     });
 
  }
+
+ window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 10) {
+    navbar.classList.add("shadow");
+  } else {
+    navbar.classList.remove("shadow");
+  }
+});
